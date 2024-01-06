153b633aa74c355b97f12e952cde17885eea314f
tajwar <tajwar.earth@proton.me>
2024-01-06 12:00:31 +0600

    add function to_cartesian

--- to_cartesian.c
+++ to_cartesian.c
@@ -1,1 +1,9 @@
 #include "to_cartesian.h"
+
+#include <math.h>
+
+void to_cartesian(double *x, double *y, double r, double phi)
+{
+	*x = r * cos(phi * M_PI / 180);
+	*y = r * sin(phi * M_PI / 180);
+}
--- to_cartesian.h
+++ to_cartesian.h
@@ -1,4 +1,6 @@
 #ifndef COORDINATE_CONVERTER_TO_CARTESIAN_H_
 #define COORDINATE_CONVERTER_TO_CARTESIAN_H_
 
+void to_cartesian(double *x, double *y, double r, double phi);
+
 #endif
