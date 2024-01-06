8d39681b9578d6f6de96f0c0b9c18529a5792aee
tajwar <tajwar.earth@proton.me>
2024-01-06 12:37:58 +0600

    add ui

--- coordinate_converter.c
+++ coordinate_converter.c
@@ -7,5 +7,38 @@
 
 int main()
 {
+	double r, phi, x, y;
+	char ui;
+
+	do {
+		printf("-- Select conversion --\n");
+		printf("[1] Cylindrical to cartesian\n");
+		printf("[2] Cartesian   to cylindrical\n");
+		printf("Select: ");
+		scanf(" %c", &ui);
+
+		if (ui != '1' && ui != '2')
+			printf("\nInvalid input!\n\n\n");
+	} while (ui != '1' && ui != '2');
+
+	switch (ui) {
+	case '1':
+		printf("r: ");
+		scanf("%lf", &r);
+		printf("phi: ");
+		scanf("%lf", &phi);
+		to_cartesian(&x, &y, r, phi);
+		printf("\nx = %lf\ny = %lf\n", x, y);
+		break;
+	case '2':
+		printf("x: ");
+		scanf("%lf", &x);
+		printf("y: ");
+		scanf("%lf", &y);
+		to_cylindrical(&r, &phi, x, y);
+		printf("\nr   = %lf\nphi = %lf\n", r, phi);
+		break;
+	}
+
 	return 0;
 }
