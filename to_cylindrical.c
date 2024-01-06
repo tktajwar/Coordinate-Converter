02ae3d0f2ee460b309b14677db4b530cf6c4fdd0
tajwar <tajwar.earth@proton.me>
2024-01-06 12:00:57 +0600

    add function to_cylindrical

--- to_cylindrical.c
+++ to_cylindrical.c
@@ -1,1 +1,9 @@
 #include "to_cylindrical.h"
+
+#include <math.h>
+
+void to_cylindrical(double *r, double *phi, double x, double y)
+{
+	*r = sqrt(pow(x, 2) + pow(y, 2));
+	*phi = 1 / tan(y / x);
+}
--- to_cylindrical.h
+++ to_cylindrical.h
@@ -1,4 +1,6 @@
 #ifndef COORDINATE_CONVERTER_TO_CYLINDRICAL_H_
 #define COORDINATE_CONVERTER_TO_CYLINDRICAL_H_
 
+void to_cylindrical(double *r, double *phi, double x, double y);
+
 #endif
